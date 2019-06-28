25e91f22625d14d89e3fa48528621be49869877f
Joost Plattel <jsplattel@gmail.com>
2019-06-28 22:32:44 +0200

    file input now working with svelte

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,3 +1,23 @@
 <svelte:head><title>Eerste Hulp Bij Abonnementen - EHBA</title></svelte:head>
 
 <h1>Eerste Hulp Bij Abonnementen</h1>
+
+<label>
+    <input type="file" multiple accept=".xls,.xlsx,.csv" bind:files>
+</label>
+
+<ul>
+    {#each (files || []) as file}
+        <li>
+            {file.name}
+        </li>
+    {/each}
+</ul>
+
+<script>
+    let files = [];        
+
+    // async function getAbonnement(files) {
+    //     await 
+    // }
+</script> 
